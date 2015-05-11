94e9d2c1004221291a1848bce335f5983a6c3a56
alaypatel07 <alay1431@gmail.com>
2015-05-11 17:31:01 +0530

    Bug fix on form daya

--- src/uploadhandler.py
+++ src/uploadhandler.py
@@ -13,11 +13,11 @@ class UploadHandler(BaseHandler):
     @coroutine
     def post(self, *args, **kwargs):
 
+        for file in self.request.files:
+            print(file)
         print(self.request)
-        token = self.get_argument('token')
-        module = self.get_argument('moduleName')
-        # token = "288412e0d8bbbe7a471116370590c520"
-        # module = "template"
+        token = "288412e0d8bbbe7a471116370590c520"
+        module = "template"
         client = AsyncCouch('logged_in_users', BaseHandler.db_url)
         query = "function(doc){if(doc.token == '" + token + "'){emit(doc, null)}}"
         view_doc = dict()
